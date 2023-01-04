5563aa7a6720cb34202ace1e51f4273fe8122fe1
Nguyen Duy Thai <92375607+Tox1cCoder@users.noreply.github.com>
2023-01-04 18:31:03 +0700

    Update 300. Longest Increasing Subsequence.cpp

--- 300. Longest Increasing Subsequence.cpp
+++ 300. Longest Increasing Subsequence.cpp
@@ -4,14 +4,14 @@ public:
         if (nums.empty()) {
             return 0;
         }
-        std::vector<int> dp(nums.size(), 1);
+        vector<int> dp(nums.size(), 1);
         for (int i = 0; i < nums.size(); i++) {
             for (int j = 0; j < i; j++) {
                 if (nums[j] < nums[i]) {
-                    dp[i] = std::max(dp[i], dp[j] + 1);
+                    dp[i] = max(dp[i], dp[j] + 1);
                 }
             }
         }
-        return *std::max_element(dp.begin(), dp.end());
+        return *max_element(dp.begin(), dp.end());
     }
 };
